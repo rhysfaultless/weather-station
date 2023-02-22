2acf8724a21f993d9b949d8b95bf53a12c97d02f
Rhys Faultless <rhysfaultless@gmail.com>
2023-02-22 10:44:31 -0500

    Added rainfall colume counting, with a 5 second sampling interval.

---
+++ software_raspberry_pi/rainfall.py
@@ -0,0 +1,30 @@
+import gpiozero
+import time
+
+rain_sensor_gpio_pin = gpiozero.Button("BOARD18")
+sampling_interval_time = 5 # measurements taken every 5 seconds
+volume_per_rainfall_bucket = 0.2794 # millimetres^3 per bucket tip. Refer to hardware datasheet.
+rainfall_buckets_counted = 0
+rainfall_volume_counted = 0
+
+def rainfall_bucket_tipped():
+    global rainfall_buckets_counted
+    rainfall_buckets_counted += 1
+    rainfall_volume_counted = rainfall_buckets_counted * volume_per_rainfall_bucket
+
+def reset_rainfall_buckets_counted():
+    global rainfall_buckets_counted
+    rainfall_buckets_counted = 0
+
+def reset_rainfall_volume_counted():
+    global rainfall_volume_counted
+    rainfall_volume_counted = 0
+
+while True:
+    start_time = time.time()
+
+    while time.time() <= start_time + sampling_interval_time:
+        rain_sensor_gpio_pin.when_pressed = rainfall_bucket_tipped
+
+    reset_rainfall_buckets_counted()
+    reset_rainfall_volume_counted()
